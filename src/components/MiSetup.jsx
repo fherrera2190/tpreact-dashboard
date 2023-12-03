debef525d6356c33a4b84c316ff6f142e8c4d962
Fernando Herrera <herrera.fernando1209@gmail.com>
2023-12-03 16:52:12 -0300

    front end movies done!

---
+++ src/components/MiSetup.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+// Dentro del archivo Setup.js
+function MiSetup(props) {
+  return (
+    <div>
+      <ul>
+        {props.listado.map((elemento, i) => <li key={elemento + i}>{elemento}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+MiSetup.propTypes = {
+  listado: PropTypes.array
+};
+
+export default MiSetup;
